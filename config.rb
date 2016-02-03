fbf15bb72c1d122b7c00f333c56c622b49082c95
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2016-02-03 17:25:34 -0200

    Added polymer settings and banner component
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>
    Signed-off-by: Arthur Jahn <stutrzbecher@gmail.com>

---
+++ config.rb
@@ -0,0 +1,4 @@
+require 'compass/import-once/activate'
+
+css_dir = "./public/components/css"
+sass_dir = "./sass"
---
+++ landing-page.php
@@ -0,0 +1,14 @@
+<?php /* Template Name: Landing Page Template */ ?>
+
+<head>
+  <script src="<?php bloginfo('template_url'); ?>/bower_components/webcomponentsjs/webcomponents-lite.min.js"></script>
+
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+  <meta name="mobile-web-app-capable" content="yes">
+  <meta name="apple-mobile-web-app-capable" content="yes">
+  <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/style.css">
+  <link rel="import" href="http://msfernandes.local/pencillabs/wp-content/themes/pencillabs-theme/public/components/pencil-banner.html">
+</head>
+<body>
+  <test-component></test-component>
+</body>
