75d476541efce5c97b1dbdd547010e4ae1409263
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2016-02-25 00:48:03 -0300

    Finished first version
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- landing-page.php
+++ landing-page.php
@@ -6,15 +6,19 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
   <meta name="mobile-web-app-capable" content="yes">
   <meta name="apple-mobile-web-app-capable" content="yes">
+  <title>Pencil Labs | Desenhando o futuro. Juntos.</title>
   <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/style.css">
   <link rel="import" href="<?php bloginfo('template_url'); ?>/public/components/pencil-banner.html">
   <link rel="import" href="<?php bloginfo('template_url'); ?>/public/components/pencil-card-container.html">
   <link rel="import" href="<?php bloginfo('template_url'); ?>/public/components/pencil-business-container.html">
+  <link rel="import" href="<?php bloginfo('template_url'); ?>/public/components/pencil-why.html">
   <link rel="import" href="<?php bloginfo('template_url'); ?>/public/components/pencil-work-container.html">
+  <link rel="import" href="<?php bloginfo('template_url'); ?>/public/components/pencil-footer.html">
 </head>
 <body>
   <pencil-banner></pencil-banner>
   <pencil-card-container></pencil-card-container>
   <pencil-business-container></pencil-business-container>
   <pencil-work-container></pencil-work-container>
+  <pencil-footer></pencil-footer>
 </body>
